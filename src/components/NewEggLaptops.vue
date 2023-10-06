7302049c3ac4547da6a8029d90cb90c7684a32c3
MoldyChip <moldychip@hotmail.com>
2023-10-05 20:15:39 -0400

    scrapper is working for newegg website

---
+++ src/components/NewEggLaptops.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="newEgg-container">
+    <h1>NewEgg's Lowest Gaming Laptops</h1>
+    <div class="card-container">
+  <div class="laptop-card" v-for="(laptop, index) in lowLaptops" :key="index">
+    <a class="anchor-card" :href='laptop.link'>
+      <div class="card-content">
+    <img class="laptop-image" :src='laptop.image' alt="laptop image" />
+    <h5 v-text="laptop.title" class="card-title"></h5>
+     </div>
+     <div class="card-details">
+    <p>Eggs {{ laptop.eggs }}  {{ laptop.reviews }}</p>
+    <span>Price {{ laptop.price }}</span>
+  </div>
+  </a>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+import * as cheerio from 'cheerio';
+import neweggServices from '@/services/NeweggServices';
+export default {
+    data() {
+        return {
+            lowLaptops: [],
+        }
+    },
+    methods: {
+         async fetchLowLaptops() {
+             await neweggServices.listLowLaptops().then((response) => {
+                let html = response.data;
+                let $ = cheerio.load(html);
+                const dataArray = [];
+                $('div.item-cell div.item-container').each(function() {
+                    const image = $(this).find('img').attr('src');
+                    const title = $(this).find('img').attr('title');
+                    const link = $(this).find('a').attr('href');
+                    const price = $(this).find('li.price-current strong').text() + $(this).find('li.price-current sup').text();
+                    const eggs = $(this).find('a.item-rating i').attr('aria-label');
+                    const reviews = $(this).find('a.item-rating span').text();
+                     dataArray.push({
+                         'title': title,
+                         'image': image,
+                         'link': link,
+                         'price': price,
+                         'eggs': eggs,
+                         'reviews': reviews
+                     });
+                });
+                console.log(dataArray);
+                this.lowLaptops = dataArray;
+            })
+            .catch((error) => {
+          if (error.response) {
+            // error.response exists
+            // Request was made, but response has error status (4xx or 5xx)
+            console.log("Error fetching laptops", error.response.status);
+          } else if (error.request) {
+            // There is no error.response, but error.request exists
+            // Request was made, but no response was received
+            console.log(
+              "Error getting laptops: unable to communicate with server"
+            );
+          } else {
+            // Neither error.response and error.request exist
+            // Request was *not* made
+            console.log("Error getting laptops: make request");
+          }
+        });
+        }
+    },
+   async created() {
+       await this.fetchLowLaptops();
+    }
+}
+
+</script>
+
+<style scoped>
+.laptop-card {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  align-items: center;
+  margin: 10px;
+}
+.anchor-card {
+  text-decoration: none;
+  text-align: center;
+}
+.card-container {
+  display: flex;
+  flex-wrap: wrap; /* Allow items to wrap to the next line */
+  justify-content: center; /* Center the content horizontally */
+  align-items: flex-start; /* Align items to the top of the container */
+}
+.card-content {
+  margin-bottom: 10px; /* Add space between image and title */
+}
+
+.card-details {
+  text-align: center;
+}
+.card-title {
+  width: 200px;
+}
+img {
+  height: 100px;
+  max-width: 100%;
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <newEgg msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/ScrapeWeb2.vue'
+import newEgg from '@/components/NewEggLaptops.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    newEgg
   }
 }
 </script>
