6308f73e06a022d877ce68d65849c79b9a313417
MoldyChip <moldychip@hotmail.com>
2023-10-06 22:12:53 -0400

    reverted ip address

--- src/services/NeweggServices.js
+++ src/services/NeweggServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     listLowLaptops() {
-        return axios.get('http://3.137.202.54:3000/newEgg',{      
+        return axios.get('http://localhost:3000/newEgg',{      
             headers: {
             'Access-Control-Allow-Origin': '*',
           },
